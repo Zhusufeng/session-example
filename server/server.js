f76cccc7e3e515c4b2c7e0d46f56bb1fca3521fd
Lisa Gee <zhusufeng@gmail.com>
2017-12-29 14:14:19 -0800

    Add code to handle post req to signup

--- server/server.js
+++ server/server.js
@@ -19,6 +19,31 @@ const users = {
   }
 };
 
+app.post('/signup', (req, res) => {
+  console.log(req.body.username);
+  let name = req.body.username;
+
+  let info = {
+    name: name
+  };
+
+  if(users[name]) {
+    info.error = 'You already have an account. Please use log in option.';
+    res.status(200).send(info);
+  } else {
+    users[name] = {
+      visitCount = 1
+    };
+
+    req.session.visitCount = 1;
+
+    info.totalVisitCount = users[name].visitCount;
+    info.sessionVisitCount = req.session.visitCount;
+    
+    res.status(201).send(info);
+  }
+});
+
 app.post('/login', (req, res) => {
   console.log(req.body.username);
   let name = req.body.username;
